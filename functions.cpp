02ababde3c1801d49d67dd3059f16982e5161d47
abracadabracools <anascools@gmail.com>
2020-02-20 17:07:48 +0530

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -1,45 +1,61 @@
-#include "functions.h"
-#include <fstream>
-
-#include <math.h>
 #include <iostream>
+#include <fstream>
+#include <math.h>
+#include "functions.h"
+
+
 using namespace std;
 
-//using namespace std;
-
-void gauss_jacobi_implicit(twodarrays temperature, int nx, int ny, double k1, double k2, double tolerance, double totime, double dtime)
+void twodarrays:: gauss_jacobi_implicit( int nx, int ny, double k1, double k2, double tolerance, double totime, double dtime)
 {
-	for (int t = 0; t < totime; t = t + dtime)
+	int jacobi = 0;
+	for (double t = 0; t < totime; t = t + dtime)
 	{
-		for (int i = 1; i < nx-1; i++)
-		{
-			for (int j = 1; j < ny-1; j++)
+		int flag=0;
+		do
+		{ ++jacobi;
+			for (int i = 1; i < nx-1; i++)
 			{
-				int flag = 0;
-				double tprev = temperature.arr[i][j];
-				double tleft = temperature.arr[i - 1][j];
-				double tright = temperature.arr[i + 1][j];
-				double ttop = temperature.arr[i][j + 1];
-				double tbottom = temperature.arr[i][j - 1];
+				for (int j = 1; j < ny-1 ; j++)
+				{
+					 flag = 0;
+					double tprev = arr[i][j];
 
-				double tnow = temperature.arr[i][j];
+					double ttop = arr[i - 1][j];
+					double tbottom = arr[i + 1][j];
+					double tright = arr[i][j + 1];
+					double tleft = arr[i][j - 1];
 
-				double denom = 1. + 2 * k1 * k1 + 2 * k2 * k2;
+					
 
-				tnow = (tprev + (k1 * (tleft + tright)) + (k2 * (ttop + tbottom))) / (denom);
+					double denom = 1. + (2. * k1 * k1) +( 2. * k2 * k2);
 
-				double error = abs(tnow - tprev);
+					double tnow = (tprev + (k1 * (tleft + tright)) + (k2 * (ttop + tbottom))) / (denom);
 
-				if (error > tolerance)
-					flag++;
+					double error = abs(tnow - tprev);
 
-				temperature.arr[i][j] = tnow;
+					if (error > tolerance)
+					{
+						flag++;
+						cout << "Not - Convergerd \t" << "i=" << i << "j=" << j << "jacobi =" << jacobi << "time=" << t <<"error="<<error<< "\n";
+					}
+					else {
+
+						cout << "Convergerd \t" << "i =" << i << "j=" << j << "jacobi =" << jacobi<< "time=" << t << "\n";
+					}
+
+					arr[i][j] = tnow; 
+					//cout << "flag" << flag << "\t"<< "time"<< t<< "\n";
+					//cout << "error" << error<<"\n";
+					//cout << "i =" << i << "j=" << j << "temp = " << temperature.arr[i][j] << "\t";
+				}
 			}
-		}
+		} while (flag > 0);
 	}
-};
+	//cout << "jacobi" << jacobi<< "\t";
+}
 
-void printout(twodarrays temperature) {
+void twodarrays:: printout(int nx,int ny) {
 
 	ofstream outdata; // outdata is like cin
 	outdata.open("temperature.dat"); // opens the file
@@ -48,14 +64,14 @@ void printout(twodarrays temperature) {
 		exit(1);
 	}
 
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < nx; i++)
 	{
-		for (int j = 0; j < 3; j++)
+		for (int j = 0; j < ny; j++)
 		{
-			outdata << temperature.arr[i][j] << "\t";
+			outdata <<arr[i][j] << "\t";
 		}
 
 	}
 	outdata.close();
 
-};
+}
